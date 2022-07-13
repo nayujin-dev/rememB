972a8d1aeebbe75584cd7762b05f45554a1dcec4
Nayujin <00nyj@sookmyung.ac.kr>
2022-07-13 15:35:41 +0900

    feat: 다른사람의 메인페이지, 공용 타이틀 컴포넌트

--- rememb/src/App.js
+++ rememb/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
+import YourHome from "./pages/YourHome";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/others" element={<YourHome />}></Route>
     </Routes>
   );
 }
---
+++ rememb/src/components/YourHome/ToCeleb.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ToCeleb = () => {
+    const onClick=()=>{
+
+    }
+  return (
+    <div>
+      <button onClick={onClick}>나도 축하해주기</button>
+    </div>
+  );
+};
+
+export default ToCeleb;
