e03882973a1f7a8000befb02e8c46d90b4287d59
duyeong-ko <329_143@naver.com>
2022-07-21 21:08:19 +0900

    fix: 네비바 1차 구현

--- rememb/src/pages/Etc/Sample.js
+++ rememb/src/pages/Etc/Sample.js
@@ -1,13 +1,11 @@
-// 메뉴탭 컴포넌트 어떻게 뜨는지 보기 위해서 임시로 만든 페이지입니다
 
 import React from "react";
-import Button from './../../components/Modal/Button';
+import NavBar from './../../components/Modal/NavBar';
 
 const Sample = () => {
-  // const [who,setWho]=useState('');
   return (
     <div>
-      <Button />
+    <NavBar />
     </div>
   );
 };
