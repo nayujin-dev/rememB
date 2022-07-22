3e254fa6f1a60aa647585364162cdd4596e41bcc
duyeong-ko <329_143@naver.com>
2022-07-22 17:41:23 +0900

    feat: 네이버 소셜 로그인

--- rememb/src/components/Tutorial/SocialLogin.js
+++ rememb/src/components/Tutorial/SocialLogin.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import KaKaoLogin from 'react-kakao-login';
 
 const SocialLogin = () => {
-  const navigate=useNavigate();
-  const onClick=()=>{
-    navigate('');
+  const navi2=useNavigate();
+  const navi3=useNavigate();
+  const onClick2=()=>{
+    navi2('');
   }
+  const onClick3=()=>{
+    navi3('');
+  
+  }
+  useEffect(()=>{
+
+  },[])
   return (
     <div>
-      <button onClick={onClick}>카카오톡으로 로그인</button><br /><br />
-      <button onClick={onClick}>네이버로 로그인</button><br /><br />
-      <button onClick={onClick}>인스타그램으로 로그인</button><br /><br />
+      <KaKaoLogin
+        token={'f39e499eb90c27c6d0be359f8e3b2d18'}
+        redirect_uri={'http://localhost:3000/OAuth/kakao/callback/'}
+        onSuccess={console.log}
+        onFail={console.error}
+        onLogout={console.info}
+        className="KakaoLogin"
+        // id="kakao"
+      /><br /><br />
+      <button onClick={onClick2}>네이버로 로그인</button><br /><br />
+      <button onClick={onClick3}>인스타그램으로 로그인</button><br /><br />
     </div>
   );
 };
