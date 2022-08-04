a2bd437c98a095119329b7c18210dca39a6f5e0b
Nayujin <00nyj@sookmyung.ac.kr>
2022-08-04 15:35:16 +0900

    fix: 디자인 요소 수정

--- rememb/src/components/CommonHome/MainTitle.js
+++ rememb/src/components/CommonHome/MainTitle.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const Who=styled.div`
+  margin-top: 10px;
+  font-size:30px;
+  font-weight: 600;
+  text-align: center;
+`;
+const Dday=styled(Who)`
+  margin:5px;
+  font-size: 20px;
+`;
 const MainTitle = () => {
     const [name,setName]=useState("멋사");
     const [bday,setBday]=useState(3);
 
   return (
-    <div>
-      <h1>{name}의 생일!</h1>
-      <h3>D - {bday}</h3>
-    </div>
+    <>
+      <Who>{name}의 생일!</Who>
+      <Dday>D - {bday}</Dday>
+    </>
   );
 };
 
