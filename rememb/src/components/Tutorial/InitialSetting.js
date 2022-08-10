4077d603bcb12149931dc8d620b5d3b3334281f6
duyeong-ko <329_143@naver.com>
2022-08-10 15:54:24 +0900

    fix: 세팅페이지 css 수정

--- rememb/src/components/Tutorial/InitialSetting.js
+++ rememb/src/components/Tutorial/InitialSetting.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import '../../style1.css';
 
 const InitialSetting = () => {
-  return <div>아</div>;
+  return (
+    <>
+      <div class="first">아아 제발 나와라</div>
+    </>
+  );
 };
 
 export default InitialSetting;
