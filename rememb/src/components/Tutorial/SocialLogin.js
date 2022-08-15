98116344710b28922784035d3aad450395135db1
duyeong-ko <329_143@naver.com>
2022-08-16 08:45:21 +0900

    fix: 로그인 페이지 디자인

--- rememb/src/components/Tutorial/SocialLogin.js
+++ rememb/src/components/Tutorial/SocialLogin.js
@@ -1,7 +1,34 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import KaKaoLogin from 'react-kakao-login';
 import { useNavigate } from 'react-router-dom';
 import Naver from './Naver';
+
+const Header = styled.div`
+  background-color: #ffeff3;
+  height: 45rem;
+  padding-top: 20rem;
+`;
+const Div = styled.div``;
+const Img = styled.img`
+  width: 20rem;
+  height: 20rem;
+`;
+const InfoText = styled.div`
+  font-size: 5rem;
+  color: #787878;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px;
+  padding-top: 25px;
+  padding-bottom: 50px;
+`;
+
+const NaverDiv = styled.div`
+  margin-top: 25px;
+`;
+
+// const KaKaoLogin = styled.div``;
 
 const SocialLogin = () => {
   const navigate = useNavigate();
@@ -19,7 +46,12 @@ const SocialLogin = () => {
   };
 
   return (
-    <div>
+    <Div>
+      <Header>
+        <Img src="img/emoticons/logo/logo2.png" />
+      </Header>
+      <InfoText>로그인 후 이용 가능합니다.</InfoText>
+
       <KaKaoLogin
         kakao_auth_url="https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code"
         token={'60176c9773f08d1a5ad7286c25a1b6d2'}
@@ -33,10 +65,12 @@ const SocialLogin = () => {
         getProfile={true}
       />
       {/* <Kakao /> */}
-      <Naver />
-      <br />
-      <br />
-    </div>
+      <NaverDiv>
+        <Naver />
+        <br />
+        <br />
+      </NaverDiv>
+    </Div>
   );
 };
 
