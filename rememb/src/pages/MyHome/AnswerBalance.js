df2d77c44dfe2398af93332abe429a5003f7d293
Nayujin <00nyj@sookmyung.ac.kr>
2022-08-16 14:34:16 +0900

    feat: 밸런스 답하기 페이지 완료

--- rememb/src/pages/MyHome/AnswerBalance.js
+++ rememb/src/pages/MyHome/AnswerBalance.js
@@ -4,82 +4,105 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const BalCon=styled.div`
-  background-color: #D9D9D9;
-  width: 90rem;
   margin: auto;
-  margin-top: 10vh;
-  text-align: center;
-  /* margin-bottom: 20px; */
-  /* position: relative; */
-  padding: 10px;
+  margin-top: 8vh;
   height: 50vh;
 `;
 const BalA=styled.div`
-  /* position: absolute; */
-  width: fit-content;
-  text-align: center;
-  /* margin: auto; */
-  font-size: 20px;
-  text-align:center;
+  display: flex;
+  margin: auto;
+  justify-content: space-evenly;
 `;
 const BalQ=styled.div`
+  margin-bottom: 5vh;
   text-align: center;
-  /* position: absolute; */
   font-weight: 500;
-  top: 10%;
-  font-size: 30px;
+  font-size: 7rem;
 `;
 const Answer=styled.button`
-  display: block;
-  margin: auto;
-  border: none;
+  position: relative;
+  font-size: 5rem;
+  font-weight: 500;
+  border: ${props=>props.back?'none':"1.5px solid #828282"};
   text-align: center;
-  width: 100%;
-  background-color: #F4F4F4;
-  margin-bottom:20px;
-  padding: 5px 15px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 30px;
-  &:active{
-   box-shadow: 1px 1px 0 rgb(0,0,0,0.5);
-   position: relative;
-   top:2px;
-  };
-  &:focus{
-   position: relative;
-   top:2px;
-   background-color: #C7C5C5;
-  }
+  width: 39rem;
+  height: 30vh;
+  background-color: ${props=>props.back?"#FFE0E8":"#FFFFFF"};
+  border-radius: 20px;
+  
+`;
+const Check=styled.img`
+  width: 7rem;
+  position: absolute;
+  top: 4rem;
+  left: 4rem;
+`;
+const Img=styled.img`
+  width: 20rem;
+  margin: 9rem 0;
 `;
 const Btn=styled.div`
   text-align: center;
   margin: auto;
-  margin-top: 20px;
-  width: 150px;
-  background-color: #D9D9D9;
-  font-size: 20px;
+  margin-top: 8rem;
+  padding: 2rem 4rem;
+  width: 30rem;
+  font-size: 5rem;
+  border-radius: 80px;
+  background-color: #FFEFF3;
+`;
+const Warn=styled.div`
+  font-size: 4.5rem;
+  font-weight: 500;
+  color: #545454;
 `;
 const AnswerBalance = () => {
   const navi=useNavigate();
   const [myA,setMyA]=useState('');
-  const onBtnClick=(e)=>{
-    setMyA(e);
+  const [left,setLeft]=useState(false);
+  const [right,setRight]=useState(false);
+  const [leftCheck,setLeftCheck]=useState('/img/check.png');
+  const [rightCheck,setRightCheck]=useState('/img/check.png');
+  const onLeftClick=()=>{
+    setMyA(1);
+    setLeftCheck('/img/pinkcheck.png');
+    setRightCheck('/img/check.png');
+    setLeft(true);
+    setRight(false);
+  }
+  const onRightClick=()=>{
+    setMyA(2);
+    setLeftCheck('/img/check.png');
+    setRightCheck('/img/pinkcheck.png');
+    setLeft(false);
+    setRight(true);
   }
   const onClick=()=>{
-    // console.log(myA);
     alert('저장되었습니다');
-    navi('/myParty/seeBalance');
+    // navi('/myParty/seeBalance');
   }
   return (
     <Layout>
       <BalCon>
-      <BalQ>케이크는 자고로</BalQ>
+      <BalQ>케이크는?</BalQ>
       <BalA>
-        <Answer onClick={()=>onBtnClick(0)}>달달하고 맛있어야 한다</Answer>
-        <Answer onClick={()=>onBtnClick(1)}>귀엽고 깜찍해야 한다</Answer>
+        <Answer onClick={onLeftClick} back={left} >
+          <Check src={leftCheck}/>
+          <Img src="/img/balanceIcon/6-1 1.png"/>
+          <br/>맛이 중요
+        </Answer>
+        <Answer onClick={onRightClick}  back={right}>
+          <Check src={rightCheck}/>
+          <Img src="/img/balanceIcon/6-2 1.png"/>
+          <br/>디자인이 중요
+        </Answer>
       </BalA>
       </BalCon>
-      <Btn onClick={onClick}>SAVE</Btn>
+      <Warn>
+        한 번 정한 대답은 변경할 수 없습니다. <br/>
+        신중하게 선택해 주세요.
+      </Warn>
+      <Btn onClick={onClick}>저장</Btn>
     </Layout>
   )
 };
