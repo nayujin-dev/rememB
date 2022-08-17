03acbad3d9802a1e5e4edaed468a08f0834f3595
duyeong-ko <329_143@naver.com>
2022-08-17 13:09:19 +0900

    feat: 소개페이지 #1

--- rememb/src/components/Etc/ShowInfo.js
+++ rememb/src/components/Etc/ShowInfo.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React from 'react';
+import Inroduce from './Introduce';
 
 const ShowInfo = () => {
   return (
     <>
-      여기는 내 정보 확인 페이지입니다.<br /><br />
-      내 계정 정보<br />
-      : 블라블라<br />
-      <br /><br />
-      내 생일 정보<br />
-      : 블라블라<br /><br />
+      <Introduce />
     </>
   );
 };
--- rememb/src/pages/Etc/Intro.js
+++ rememb/src/pages/Etc/Intro.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Introduce from '../../components/Etc/Introduce';
 
 const Intro = () => {
-  return <div>rememb 설명 페이지입니다.</div>;
+  return (
+    <>
+      <Introduce />
+    </>
+  );
 };
 
 export default Intro;
