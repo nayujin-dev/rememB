1fb6462f7bbd81983f5492e366f4851490f629c4
duyeong-ko <329_143@naver.com>
2022-08-18 02:25:55 +0900

    fix: 튜토리얼 페이지 수정

--- rememb/src/pages/Tutorial/Tutorial.js
+++ rememb/src/pages/Tutorial/Tutorial.js
@@ -1,13 +1,27 @@
-import React from "react";
-import MainTitle from "../../components/CommonHome/MainTitle";
-import PartyRoom from "../../components/CommonHome/PartyRoom";
-import MakeParty from "../../components/Tutorial/MakeParty";
+import React from 'react';
+import PartyRoom from '../../components/CommonHome/PartyRoom';
+import MakeParty from '../../components/Tutorial/MakeParty';
+import styled from 'styled-components';
 
+const Who = styled.div`
+  margin-top: 12rem;
+  font-size: 9rem;
+  color: #fe4179;
+  font-weight: 700;
+  text-align: center;
+`;
+const Dday = styled.div`
+  margin: 1rem 0 3rem;
+  text-align: center;
+  font-weight: 400;
+  font-size: 6rem;
+`;
 const Tutorial = () => {
   // const [who,setWho]=useState('');
   return (
     <>
-      <MainTitle />
+      <Who>멋사의 생일</Who>
+      <Dday>D - 7</Dday>
       <PartyRoom />
       <MakeParty />
     </>
