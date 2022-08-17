3041ee837b294f14b8f78adc453c449e80e176cf
Nayujin <00nyj@sookmyung.ac.kr>
2022-08-18 01:01:03 +0900

    fix: 로딩 페이지

--- rememb/src/pages/Tutorial/Setting.js
+++ rememb/src/pages/Tutorial/Setting.js
@@ -80,16 +80,16 @@ const Setting = ({ res }) => {
   return (
     <>
       {username === '' ? (
-        <>
+        <div style={{position:'absolute', top:'20vh'}}>
            <img
             src="/img/pangLogo.png"
             alt="loading"
-            style={{ width: '30rem', height: '30rem', textAlign:'center', justifyContent:'center'}}
+            style={{ width: '30rem', height: '30rem', textAlign:'center'}}
         />
         <div style={{fontsize:'6rem', textAlign:'center'}}>
           로딩 중입니다
         </div>
-        </>
+        </div>
       ) : (
         <InitialSetting username={username} birth={birth} accesstoken={access} id={id}/>
       )}
